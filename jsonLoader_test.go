a64d4f8926919e182a1a447058aa31ffb1566470
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-07-20 16:02:44 +0200

    Point to newest gojsonreference (#3)

--- jsonLoader_test.go
+++ jsonLoader_test.go
@@ -8,7 +8,8 @@ import (
 
 func TestJSONLoaderLocal(t *testing.T) {
 	for _, p := range []string{
-		"file://./extra/fragment_schema.json",
+		"file://./testdata/extra/fragment_schema.json",
+		"file://testdata/extra/fragment_schema.json",
 	} {
 		t.Run("path="+p, func(t *testing.T) {
 			_, err := NewSchema(NewReferenceLoader(p))
